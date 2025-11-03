39655da1ef2d644f87d551d025ba0af08974e856
krymar02 <kryssuni02@gmail.com>
2025-11-03 15:22:29 -0600

    Ignoro el directorio build de Laboratorio12

--- semanas/semana12/Laboratorio12/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krymar02/ie0417/semanas/semana12/Laboratorio12
-# Build directory: /home/krymar02/ie0417/semanas/semana12/Laboratorio12/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CalculatorTests "/home/krymar02/ie0417/semanas/semana12/Laboratorio12/build/test_calculator")
-set_tests_properties(CalculatorTests PROPERTIES  _BACKTRACE_TRIPLES "/home/krymar02/ie0417/semanas/semana12/Laboratorio12/CMakeLists.txt;22;add_test;/home/krymar02/ie0417/semanas/semana12/Laboratorio12/CMakeLists.txt;0;")
